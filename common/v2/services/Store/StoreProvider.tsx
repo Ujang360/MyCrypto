1b77855a54adc25b41b368904ffeba02f81f858e
blurpesec <hahn.michael.f@gmail.com>
2020-03-17 16:51:36 -0700

    scan tokens when new tx goes from pending -> success | fail

--- common/v2/services/Store/StoreProvider.tsx
+++ common/v2/services/Store/StoreProvider.tsx
@@ -79,7 +79,7 @@ export const StoreContext = createContext({} as State);
 export const StoreProvider: React.FC = ({ children }) => {
   const {
     accounts: rawAccounts,
-    addNewTransactionToAccount,
+    addNewTransactionToAccountAndUpdateTokens,
     getAccountByAddressAndNetworkName,
     updateAccountAssets,
     updateAccountsBalances,
@@ -183,7 +183,7 @@ export const StoreProvider: React.FC = ({ children }) => {
               pendingTransactionObject.senderAccount ||
               getAccountByAddressAndNetworkName(receipt.from, pendingTransactionObject.network.id);
 
-            addNewTransactionToAccount(senderAccount, {
+            addNewTransactionToAccountAndUpdateTokens(senderAccount, {
               ...receipt,
               timestamp: txTimestamp,
               stage: txStatus
